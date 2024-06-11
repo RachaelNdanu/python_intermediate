aa3eb1a90e48fb371cc4de9ded1ff34c12aa004f
RachaelNdanu <rndanu102@gmail.com>
2024-06-11 15:26:54 +0300

    advanced changes

--- json_tutorial.py
+++ json_tutorial.py
@@ -1,10 +1,19 @@
 import json
 
-person ={"name": "John" , "age":30 , "city": "NewYork" , "hasChildren": False , "titles":["engineer", "programmer"]}
+person ={"name": "John" , "age": 30 , "city": "NewYork" , "hasChildren": False , "titles":["engineer", "programmer"]}
 
 
 personJson = json.dumps(person , indent=4 , sort_keys=True)
-print(personJson)
+#print(personJson)
 
 with open('person.json', 'w') as file:
     json.dump(person,file ,indent=4)
+
+
+'''person = json.loads (personJson)
+print(person)'''
+
+
+with open('person.json' , 'r') as file:
+    person = json.load(file)
+    print(person)
---
+++ random_examples.py
@@ -0,0 +1,5 @@
+import random
+
+mylist = list ("ABCDEFGH")
+a = random.choices(mylist , k=3)
+print(a)
